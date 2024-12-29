689ab873087b6c725284a63ec4111ba8c8b50717
code-eater-piyush <kpiyush032005@gmail.com>
2024-12-29 22:23:20 +0530

    initial commit

---
+++ cpp-elseif.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main() {
+    int age ;
+    cin>> age ;
+    if (age <18) {
+        cout << "valid" ;
+
+    } 
+    else if (age>18) {
+        cout << "not valid" ;
+    }
+    else {
+        cout<< "lol" ;
+
+    }
+    return 0 ;
+}
---
+++ cppifelse.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int main() {
+    int age ;
+    cin>> age ;
+    if (age<= 18) {
+        cout << "valid" ;
+    }
+
+    else {
+        cout << "not vaid" ;
+     }
+     return 0 ;
+}
